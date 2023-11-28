15b16560fc71035a37516d210d6e073e546131d8
Luis Cisneros <tiko.cisneros@gmail.com>
2023-11-27 22:05:31 -0500

    build: project configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Center } from '../styled-system/jsx';
+
+function App() {
+  return (
+  <Center h="100vh" fontSize="xxx-large" fontWeight={600} bg="yellow.200">
+    Hello 🐼!
+  </Center>);
+}
+
+export default App;
